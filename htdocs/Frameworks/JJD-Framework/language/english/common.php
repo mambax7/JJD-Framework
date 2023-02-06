bdba382c565b91e2da7fa38c80e70b38124b4d7e
JJDai <jjdelalandre@orange.fr>
2023-02-06 23:04:02 +0100

    constantes de langue en

--- htdocs/Frameworks/JJD-Framework/language/english/common.php
+++ htdocs/Frameworks/JJD-Framework/language/english/common.php
@@ -9,5 +9,17 @@
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 */
 
+\define('_CO_JJD_LOCALE_MONTH', "janvier,février,mars,avril,mai,juin,juillet,août,septembre,octobre,novembre,décembre,lundi,mardi,mercredi,jeudi,vendredi,samedi,dimanche");
+\define('_CO_JJD_ENGLISH_MONTH', "January,February,March,April,May,June,July,August,September,October,November,December,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday");
+
+
+\define('_CO_JJD_NB_QUIZ_2_list', "Nombre d'items maximum");
+\define('_CO_JJD_NAME_LENGTH', "Longueur des titres");
+\define('_CO_JJD_CATEGORIES', "Catégories");
+\define('_CO_JJD_ALL_CAT', "Toutes catégories");
+\define('_CO_JJD_BLOCK_TITLE', "Titre du block");
+\define('_CO_JJD_BLOCK_DESC', "Description du block");
+
+
 
 ?>
